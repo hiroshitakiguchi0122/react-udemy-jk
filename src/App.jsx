499d319f5600047920a5c0fd5c7fa734530b9ec1
hiroshi <79740508+hiroshitakiguchi0122@users.noreply.github.com>
2022-07-15 15:57:00 +0900

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  const onClickButton = () => alert();
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
+  return (
+    <>
+      {/* ←React.Fragmentの省略 */}
+      <h1 style={{ color: "red" }}>こんにちは！</h1>
+      <p style={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
+    </>
+  );
+};
+
+export default App;
